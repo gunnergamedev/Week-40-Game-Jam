126308de7bda26f8887659be64bbd7e8ba8408df
Tshegofatso <33226065+Holland993@users.noreply.github.com>
2018-04-19 13:05:24 +0200

    Puzzle 3
    The commit that has most of Puzzle 3

---
+++ Assets/Scripts/Level3/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    private Piece[] pieces;
+    private int correctPieceCount;
+    private static string FindSceneObjectsOfTag;
+
+    private void Start()
+    {
+        pieces = FindObjectsOfType<Piece>();
+    }
+
+    public void CheckTiles()
+    {
+        foreach (Piece Piece in pieces)
+        {
+            if (Piece.values[0] == 1)
+            {
+                Debug.Log(pieces + " is right");
+                correctPieceCount++;
+                //Play musics 
+            }
+        }
+
+        if (correctPieceCount == pieces.Length)
+        {
+            //WIN CODE
+            Debug.Log("cheese");
+        }
+    }
+}
---
+++ Assets/Scripts/Level3/Piece.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour
+{
+
+
+    public int[] values = { 1, 0, 0, 0 };
+    public float speed;
+    float realRotation;
+
+    private Piece[] pieces;
+    private int correctPieceCount;
+
+    public GameManager gm;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (transform.eulerAngles.z != realRotation)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, realRotation), speed);
+        }
+        gm.CheckTiles();
+
+    }
+
+
+
+    void OnMouseDown()
+    {
+
+        RotatePiece();
+        gm.CheckTiles();
+
+    }
+
+    public void RotatePiece()
+    {
+        realRotation += 90;
+
+        if (realRotation == 360)
+        {
+            realRotation = 0;
+        }
+
+        RotateValues();
+    }
+
+
+
+    public void RotateValues()
+    {
+
+        int temp = values[0];
+
+        for (int i = 0; i < values.Length - 1; i++)
+        {
+            values[i] = values[i + 1];
+        }
+        values[3] = temp;
+
+        gm.CheckTiles();
+    }
+
+    public void CheckTiles()
+    {
+        foreach (Piece piece in pieces)
+        {
+            if (piece.values[0] == 1)
+            {
+                Debug.Log(piece.name + " is right");
+                correctPieceCount++;
+                //Play musics 
+            }
+        }
+
+        if (correctPieceCount == pieces.Length)
+        {
+            //WIN CODE
+            Debug.Log("cheese");
+        }
+    }
+
+
+}
